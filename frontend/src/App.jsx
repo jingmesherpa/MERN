61c7a30d6c3b5356e38fdba89a1bb4d8543f4943
jingmesherpa <jigmesherpa9862@gmail.com>
2026-01-22 19:04:27 +0545

    Frontend Setup, Components

---
+++ frontend/src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Home from './components/Home'
+import About from './components/About'
+import Courses from './components/Courses'
+import Footer from './components/Footer'
+import nav from './components/nav'
+
+const App = () => {
+  return (
+    <div>
+      hwllo from app.jsx
+      
+      <Home></Home>
+      <About></About>
+      <Courses></Courses>
+      <Footer></Footer>
+      <nav></nav>
+    </div>
+  )
+}
+
+export default App
---
+++ frontend/src/components/nav.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const nav = () => {
+  return (
+    <div>
+      hello from nav
+    </div>
+  )
+}
+
+export default nav
